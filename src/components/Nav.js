0e6118c9297c84b1d763f7fed98c9ee761b62aa6
Mohamed <boussbaa2004@gmail.com>
2024-09-20 09:51:55 +0100

    premier commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,12 +4,14 @@ import './Nav.css'
 
 export default function Nav() {
   return (
-    <div >
-        <ul >
-            <li><Link to="/">Acceill</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/produit">Produit</Link></li>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div  >
+      <ul  className="navbar-nav ">
+            <li className="nav-item active mx-auto p-2"><Link to="/">Acceill</Link></li>
+            <li className="nav-item active mx-auto p-2"><Link to="/about">About</Link></li>
+            <li className="nav-item active mx-auto p-2"><Link to="/produit">Produit</Link></li>
         </ul>
-    </div>
+      </div>
+    </nav>
   )
 }
